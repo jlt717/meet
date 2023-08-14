1564fb63e6f1f4a9bfe3b01ec2c6678a0836eb0b
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-08-14 08:09:50 -0500

    added Warning Alert

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import { extractLocations, getEvents } from "./api";
 import NumberOfEvents from "./components/NumberOfEvents";
-import { InfoAlert, ErrorAlert } from "./Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import "./App.css";
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
   const [eventNumber, setEventNumber] = useState(32);
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,6 +26,11 @@ const App = () => {
         console.log(error);
       }
     };
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("Its looks like you're offline.");
+    }
     fetchData();
   }, []);
 
@@ -52,6 +58,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -47,6 +47,11 @@ class ErrorAlert extends Alert {
     this.bgColor = "rgb(255, 127, 127)"; //light red
   }
 }
-
-export { InfoAlert, ErrorAlert };
-
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "rgb(255, 165, 0)"; //orange
+    this.bgColor = "rgb(255, 213, 128)"; //light orange
+  }
+}
+export { InfoAlert, ErrorAlert, WarningAlert };
